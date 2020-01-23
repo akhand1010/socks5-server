9d28f645b127dc2a152a0d11acd1796e3adea65d
Vladislav Kamenev <mazahakaforever@ya.ru>
2020-01-23 05:24:46 +0300

    Initial

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:1.13-alpine as builder
+
+WORKDIR /go/src/github.com/vlakam/socks5-server
+RUN apk add --no-cache git
+COPY . .
+RUN go get ./...
+RUN GOARCH=amd64 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-s' -o ./server
+
+FROM scratch
+COPY --from=builder /go/src/github.com/vlakam/socks5-server/server /
+ENTRYPOINT ["/server"]
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	socks5 "github.com/vlakam/go-socks5"
+	"log"
+	"os"
+)
+
+func main() {
+	socks5conf := &socks5.Config{
+		AuthMethods: []socks5.Authenticator{&socks5.NoAuthAuthenticator{}},
+	}
+
+	server, err := socks5.New(socks5conf)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	log.Print("Starting server on 1080 port")
+	err = server.ListenAndServe("tcp", "0.0.0.0:1080")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+}
